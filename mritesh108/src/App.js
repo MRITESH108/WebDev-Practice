d2d4c6c719f19b6bf176b8037b1a72841d5de2ff
Ritesh Pandey <riteshpandeyvindhyawasi@gmail.com>
2025-06-30 00:46:51 +0530

    Hey let's build dream project

---
+++ mritesh108/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+          <Navbar title="MRITESH108" navAbout="About Us" />
+          
+    </>
+  );
+}
+
+
+
+
+export default App;
