ebf9f11acdb5fc0e8e698239969200a1ac51aace
Ritesh Pandey <riteshpandeyvindhyawasi@gmail.com>
2025-06-30 01:04:23 +0530

    Change Case feature

--- mritesh108/src/App.js
+++ mritesh108/src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 
 import './App.css';
 import Navbar from './components/Navbar';
+import ChangeCase from './components/ChangeCase';
+
+
 
 function App() {
   return (
     <>
           <Navbar title="MRITESH108" navAbout="About Us" />
-          
+          <ChangeCase />
     </>
   );
 }
---
+++ mritesh108/src/components/ChangeCase.js
@@ -0,0 +1,12 @@
+
+
+export default function ChangeCase() {
+    return (
+        
+        <div className="mb-3">
+            
+            <textarea clasName="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+        </div>
+
+    )
+}
